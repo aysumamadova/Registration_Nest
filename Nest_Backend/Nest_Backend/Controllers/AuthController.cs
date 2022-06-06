34eb46f900b46507138f72ce39069c28d80e2b95
Aysu Mammadova <tu7tw2d21@aztu.local>
2022-06-06 10:03:52 +0400

    add

---
+++ Nest_Backend/Nest_Backend/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Nest_Backend.Models;
+using Nest_Backend.ViewModels.AuthLogin;
+using System.Threading.Tasks;
+
+namespace Nest_Backend.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AuthController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Register()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            AppUser nUser = new AppUser()
+            {
+
+                Name = registerVM.FirstName,
+                Surname = registerVM.LastName,
+                Email = registerVM.Email,
+                UserName = registerVM.Username
+            };
+            IdentityResult result = await _userManager.CreateAsync(nUser, registerVM.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+            }
+            await _signInManager.SignInAsync(nUser, true);
+            return RedirectToAction("Index", "Home");
+        }
+        public async Task<IActionResult> SignOut()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Register))
+        }
+    }
+}
